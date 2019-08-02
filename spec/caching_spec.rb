6b124f628c5ad5672a6c8edd0d21af1272959310
Andreas Robecke <a.robecke@getsafe.de>
2019-08-02 15:36:45 +0200

    Spec that caching can be toggled per request

--- spec/caching_spec.rb
+++ spec/caching_spec.rb
@@ -53,5 +53,16 @@ RSpec.describe NxtHttpClient::Client do
         expect(subject.fire(http_stats_url('200'))).to_not be_cached
       end
     end
+
+    it 'can be switched on per request' do
+      # we need to post here in order to not collide with cached content since we are in the same thread as the example above
+      expect(subject.post(http_stats_url('200'), cache: :thread)).to_not be_cached
+      expect(subject.post(http_stats_url('200'), cache: :thread)).to be_cached
+      expect(subject.post(http_stats_url('200'), cache: :thread)).to be_cached
+
+      # not yet cached and thus does not hit the cache the first time
+      Thread.new { expect(subject.fire(http_stats_url('200'), cache: :thread)).to_not be_cached }.join
+      Thread.new { expect(subject.fire(http_stats_url('200'), cache: :thread)).to_not be_cached }.join
+    end
   end
 end
