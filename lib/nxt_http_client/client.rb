a9df54d18c1f320936a84d820e3364a54c645b93
Andreas Robecke <a.robecke@getsafe.de>
2020-02-11 19:12:59 +0100

    Refactor

--- lib/nxt_http_client/client.rb
+++ lib/nxt_http_client/client.rb
@@ -7,10 +7,7 @@ module NxtHttpClient
       base_url = opts.delete(:base_url) || default_config.base_url
       url = [base_url, url].reject(&:blank?).join('/')
 
-      duplicated_slashes = url.match(/([^:]\/{2,})/)
-      duplicated_slashes && duplicated_slashes.captures.each do |capture|
-        url.gsub!(capture, "#{capture[0]}/")
-      end
+      url_without_duplicated_hashes(url)
 
       opts = default_config.request_options.with_indifferent_access.deep_merge(opts.with_indifferent_access)
       opts[:headers] ||= {}
@@ -19,19 +16,7 @@ module NxtHttpClient
         opts[:headers]['X-Request-ID'] ||= default_config.x_request_id_proc.call
       end
 
-      if opts[:cache] ||= false
-        strategy = opts.delete(:cache)
-
-        case strategy.to_s
-        when 'thread'
-          cache_key = Thread.current[:nxt_http_client_cache_key] ||= "#{SecureRandom.base58}::#{DateTime.current}"
-          opts[:headers].reverse_merge!(cache_key: cache_key)
-        when 'global'
-          opts[:headers].delete(:cache_key)
-        else
-          raise ArgumentError, "Cache strategy unknown: #{strategy}. Options are #{CACHE_STRATEGIES}"
-        end
-      end
+      build_cache_header(opts)
 
       Typhoeus::Request.new(url, **opts.symbolize_keys)
     end
@@ -89,6 +74,8 @@ module NxtHttpClient
       end
     end
 
+    private
+
     def dup_handler_from_class
       self.class.response_handler.dup
     end
@@ -96,5 +83,31 @@ module NxtHttpClient
     def default_config
       self.class.default_config
     end
+
+    def build_cache_header(opts)
+      if opts[:cache] ||= false
+        strategy = opts.delete(:cache)
+
+        case strategy.to_s
+        when 'thread'
+          cache_key = Thread.current[:nxt_http_client_cache_key] ||= "#{SecureRandom.base58}::#{DateTime.current}"
+          opts[:headers].reverse_merge!(cache_key: cache_key)
+        when 'global'
+          opts[:headers].delete(:cache_key)
+        else
+          raise ArgumentError, "Cache strategy unknown: #{strategy}. Options are #{CACHE_STRATEGIES}"
+        end
+      end
+    end
+
+    def url_without_duplicated_hashes(url)
+      duplicated_slashes = url.match(/([^:]\/{2,})/)
+      duplicated_slashes && duplicated_slashes.captures.each do |capture|
+        url.gsub!(capture, "#{capture[0]}/")
+      end
+
+      url
+    end
+
   end
 end
