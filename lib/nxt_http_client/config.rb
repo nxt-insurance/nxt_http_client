7827df793272780b8a4749e896e7f7e990017d81
shalvah-gs <s.adebayo@hellogetsafe.com>
2023-06-20 17:16:52 +0100

    Require tital timeout

--- lib/nxt_http_client/config.rb
+++ lib/nxt_http_client/config.rb
@@ -19,7 +19,7 @@ module NxtHttpClient
       end
     end
 
-    def timeout_seconds(total: nil, connect: nil)
+    def timeout_seconds(total:, connect: nil)
       timeouts = { total:, connect:, }.compact
 
       self.timeouts = timeouts
